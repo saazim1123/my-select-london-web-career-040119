cba2cc5daa7270f1da8b1192839238d8b5193704
Adnan Azim <saazim1123@gmail.com>
2019-03-12 19:50:30 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,8 +4,30 @@ def my_select(collection)
  i = 0 
  while i < collection.size
   
-  empty_array << yield(collection[i])
-  i += 1
+  if yield(collection[i]) == true
+    empty_array << collection[i]
  end
  empty_array
 end
+
+=begin
+
+def my_select(array) 
+count = 0
+result = []
+
+while count < array.size
+  if yield(array[count]) == true
+  result.push(array[count])
+end
+  count+=1
+end
+result
+end
+
+
+
+
+
+
+=end
