d4abdaa1a3d183c3ba2e135f6ec08c7425699f34
Adnan Azim <saazim1123@gmail.com>
2019-03-12 19:53:18 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,9 +4,7 @@ def my_select(collection)
    i = 0 
    while i < collection.length
     
-    if yield(collection[i]) == true
-      empty_array << collection[i]
-      i += 1
+    if (yield(collection[i]))
    end
  empty_array
 end
