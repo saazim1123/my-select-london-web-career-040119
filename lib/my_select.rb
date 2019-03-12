ccff21d1a37fa7f751ab361c527be5634a2f8f1e
Adnan Azim <saazim1123@gmail.com>
2019-03-12 19:54:09 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -13,15 +13,3 @@ def my_select(collection)
 end
 
 
-def my_select(array) 
-  i =0 
-  select = [] 
-  while i < array.length 
-    if (yield(array[i]))
-      # select.push(array[i])OR 
-      select << array[i] 
-    end 
-    i += 1 
-  end 
-  select 
-end 
